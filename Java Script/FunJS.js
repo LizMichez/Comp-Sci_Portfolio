60b1031fe265ff76db8fdfccb196386b996d80fc
Liz M <lizzymicheg@gmail.com>
2018-10-17 12:06:27 -0400

    Update - JS1
    -Played with java script
    -Tried to fix picture problem

---
+++ Java Script/FunJS.js
@@ -0,0 +1,14 @@
+alert ("hello")
+var name = prompt ("What's your name?")
+if (name == "Liz")
+	alert ("ayyyyy nice to see you");
+else
+	alert ("Hello " + name);
+
+var age = prompt ("So how old are you?")
+
+function flash(){
+	var test
+	document.body.style.backgroundColor = "#AA0000";
+
+}
